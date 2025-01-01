8012df8b2a4bad3dd8e5896dd892ec1343c0c889
cyarnaud <cyril.arnaud@sekoia.io>
2025-01-01 12:06:08 -0500

    task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -10,5 +11,16 @@ def emotion_detector(text_to_analyze):
     }
 
     response = requests.post(url, headers=headers, json=input_json)
+    formatted_response = json.loads(response.text)
+    emotion_scores = formatted_response["emotionPredictions"][0]["emotion"]
+    emotion = {
+        'anger': emotion_scores['anger'],
+        'disgust': emotion_scores['disgust'],
+        'fear': emotion_scores['fear'],
+        'joy': emotion_scores['joy'],
+        'sadness':  emotion_scores['sadness']
+    }
+    dominant_emotion = max(emotion, key=emotion.get)
+    emotion['dominant_emotion'] = dominant_emotion
 
-    return response.text
+    return emotion
